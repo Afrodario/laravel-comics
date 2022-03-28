7fe7348652e689838960a24f26c2919de16d57e4
Dario La Tegola <dariolategola@gmail.com>
2022-03-28 15:35:17 +0200

    Inserito footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,48 @@
 <footer>
-    Parte di Footer
-</footer>
+
+    <div class="footer-top">
+      <div class="container">
+
+        <div class="foot-container flex">
+          <div class="footer-list">
+              <h3>DC COMICS</h3>
+              <ul>
+                  <li>PROVA</li>
+              </ul>
+              <h3>SHOP</h3>
+              <ul>
+                <li>PROVA</li>
+            </ul>
+          </div>
+          <div class="footer-list">
+              <h3>DC</h3>
+              <ul>
+                <li>PROVA</li>
+            </ul>
+          </div>
+          <div class="footer-list">
+              <h3>SITES</h3>
+              <ul>
+                <li>PROVA</li>
+            </ul>
+          </div>
+        </div>
+        <!-- <img src="../assets/img/dc-logo-bg.png"> -->
+      </div>
+    </div>
+
+    <div class="footer-bottom">
+      <div class="container flex">
+        <button>SIGN-UP NOW!</button>
+        <div class="social-panel flex">
+          <span>FOLLOW US</span>
+          <img src="{{asset('images/footer-facebook.png')}}">
+          <img src="{{asset('images/footer-periscope.png')}}">
+          <img src="{{asset('images/footer-pinterest.png')}}">
+          <img src="{{asset('images/footer-twitter.png')}}">
+          <img src="{{asset('images/footer-youtube.png')}}">
+        </div>
+      </div>
+    </div>
+      
+  </footer>
