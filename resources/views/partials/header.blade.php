bcc438114750ad56cbea8853c58ae44a2145bfbf
Dario La Tegola <dariolategola@gmail.com>
2022-03-28 15:25:52 +0200

    Prove interazione componenti

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,23 @@
 <header>
-    Parte di Header
+    <div class="container">
+        <nav class="flex">
+            <a href="#">
+                <img src="{{asset('images/dc-logo.png')}}" alt="DC Logo">
+            </a>
+            <ul class="flex">
+                <li>
+                    <a href="#">Prova</a>
+                </li>
+                <li>
+                    <a href="#">Prova</a>
+                </li>
+                <li>
+                    <a href="#">Prova</a>
+                </li>
+                <li>
+                    <a href="#">Prova</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </header>
